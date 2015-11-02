2fa72a0cb1a3ec9d5f34d0f785b7ceec1d104546
dominhhai1096 <dominhhai1096@gmail.com>
2015-11-02 14:30:12 +0700

    Initial Commit

---
+++ UIScrollView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIScrollView
+//
+//  Created by Do Minh Hai on 11/2/15.
+//  Copyright (c) 2015 Do Minh Hai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIScrollView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIScrollView
+//
+//  Created by Do Minh Hai on 11/2/15.
+//  Copyright (c) 2015 Do Minh Hai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
